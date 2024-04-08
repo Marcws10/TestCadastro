e656edeb56e3c5c2aabe89e8f77fca93ba76e609
Marcws10 <marcws.10@gmail.com>
2024-04-08 13:13:50 -0300

    asdas

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function CadastroUsuario() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleCadastro = () => {
+        axios.post('http://localhost:3001/cadastrar-usuario', {
+            username: username,
+            password: password
+        })
+        .then(response => {
+            console.log(response.data);
+            alert('Usuário cadastrado com sucesso!');
+        })
+        .catch(error => {
+            console.error('Erro ao cadastrar usuário:', error);
+            alert('Erro ao cadastrar usuário. Por favor, tente novamente.');
+        });
+    };
+
+    return (
+        <div>
+            <h2>Cadastro de Usuário</h2>
+            <label>Username:</label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <br />
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <button onClick={handleCadastro}>Finalizar cadastro</button>
+        </div>
+    );
 }
 
-export default App;
+export default CadastroUsuario;
